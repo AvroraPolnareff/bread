71e74cf15c4a30ca1199e63953b231427e173f77
SequenceLin <SequenceLin@gmail.com>
2020-09-01 16:17:14 +0300

    🎨 fixed test file name

--- src/tests/RivenDetailsParser.test.ts
+++ src/tests/RivenDetailsParser.test.ts
@@ -1,7 +1,7 @@
-import {RivenPageParser} from "../parsers/RivenParser";
+import {RivenDetailsParser} from "../parsers/RivenParser";
 import {rivenPage} from "./testData";
 
 test("parses the page", () => {
-    const parser = new RivenPageParser(rivenPage)
+    const parser = new RivenDetailsParser(rivenPage)
     expect(parser.parse()).toEqual({"bids": [], "createdAt": "8 minutes ago", "description": "", "updatedAt": "8 minutes ago"})
 })
