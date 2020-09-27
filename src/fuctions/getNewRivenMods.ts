e8bb9e39e2072cc64c70d7458677c7e7a9052b8c
SequenceLin <SequenceLin@gmail.com>
2020-09-27 12:33:31 +0300

    refactoring: eliminating dead code: fetch data from html pages

--- src/fuctions/getNewRivenMods.ts
+++ src/fuctions/getNewRivenMods.ts
@@ -1,9 +1,6 @@
 import {getCustomRepository} from "typeorm";
 import {RivenListRepository} from "../db/repository/RivenListRepository";
-import {Browser} from "puppeteer";
 import * as _ from "lodash";
-import {JSDOM} from "jsdom";
-import {RivenListParser} from "../parsers/RivenListParser";
 import {Auction} from "../features/RivenHunter";
 import {WMAPI} from "../api/WMAPI";
 
@@ -37,37 +34,3 @@ export const getNewRivenMods = async (marketUrl: string) => {
         return []
     }
 }
-
-
-
-
-const getRivenList = async (url: string, browser: Browser) => {
-
-    const page = await browser.newPage()
-    await page.goto(url)
-    await page.click(".filter__buttons-horizontal--2aCfA > .btn")
-    const content = await page.content()
-    const height = getPageHeight(content) + 3000
-    await page.setViewport({width: 600, height})
-    await sleep(3000)
-    const allContent = await page.content()
-    await page.close()
-
-    const parser = new RivenListParser(allContent)
-    return parser.parse()
-}
-
-const getPageHeight = (page) => {
-    const dom = new JSDOM(page)
-    const style = dom.window.document.querySelector(".infinite-scroll").getAttribute('style')
-    const height = /(\d)+/g.exec(style)[0]
-    return parseInt(height)
-}
-
-function sleep(ms) {
-    return new Promise((resolve) => {
-        setTimeout(resolve, ms);
-    });
-}
-
-
