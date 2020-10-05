2f2f80bfa08d575dbfad338fb4971cfb0f080b23
AvroraPolnareff <araintheskywith@gmail.com>
2020-10-05 04:11:53 +0300

    adding market url service

--- apps/server/src/database/MarketUrl.ts
+++ apps/server/src/database/MarketUrl.ts
@@ -1,15 +1,42 @@
 import {Column, Connection, Entity, PrimaryGeneratedColumn, Repository} from "typeorm";
 import {consts} from "../common/const";
-import {Inject, Injectable} from "@nestjs/common";
+import {HttpException, HttpStatus, Inject, Injectable} from "@nestjs/common";
+import {MarketUrlDto} from "../dto/MarketUrlDto";
 
 @Injectable()
 export class MarketUrlService {
   constructor(
     @Inject(consts.MARKET_URL_REPOSITORY)
-    private marketUrlRepository: Repository<MarketUrl>
+    private urlRepository: Repository<MarketUrl>
   ) {}
 
+  async add(marketUrlDto: MarketUrlDto) {
+    const urls = await this.urlRepository.find(marketUrlDto)
+    if (urls.length) {
+      throw new HttpException('url already added', HttpStatus.CONFLICT)
+    }
+    const entity = this.urlRepository.create(marketUrlDto)
+    return await this.urlRepository.save(entity)
+  }
 
+  async delete(marketUrlDto: Partial<MarketUrlDto>) {
+    const entities = await this.urlRepository.find(marketUrlDto)
+    for (const entity of entities) {
+      await this.urlRepository.delete(entity)
+    }
+  }
+
+  async findById(id: number) {
+    const entity = await this.urlRepository.findOne({id})
+    if (!entity) {
+      throw new HttpException('url not found', HttpStatus.NOT_FOUND)
+    }
+    return entity
+  }
+
+  async find(marketUrlDto: Partial<MarketUrlDto>) {
+    return await this.urlRepository.find(marketUrlDto)
+  }
 }
 
 export const marketUrlProviders = [
